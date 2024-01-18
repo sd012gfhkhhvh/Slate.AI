c9e8d79c2a1f8c8dc3746476c0b1c6021138cf15
SOHAM DAS <91667720+sd012gfhkhhvh@users.noreply.github.com>
2024-01-18 20:44:44 +0530

    Update index.jsx

--- client/src/components/Forms/index.jsx
+++ client/src/components/Forms/index.jsx
@@ -17,14 +17,6 @@ const Form = ({ socket, setUser }) => {
       <div className="h-[25rem] mt-5 md:w-[400px]  p-5 border-2  border-blue-600 border-solid rounded-md mx-auto flex flex-col items-center">
         <h1 className="text-blue-600 font-bold">Join Room</h1>
         <div className="row h-100 pt-5">
-          <div className="form-box col-md-4 mt-5 form-box p-5 border border-primary rounded-4 mx-auto d-flex flex-column align-items-center">
-            <h1 className="text-primary fw-bold">Create Room</h1>
-            <CreateRoom socket={socket} setUser={setUser} />
-          </div>
-          <div className="form-box col-md-4 mt-5 form-box p-5 border border-primary rounded-4 mx-auto d-flex flex-column align-items-center">
-            <h1 className="text-primary fw-bold">Join Room</h1>
-            <JoinRoom socket={socket} setUser={setUser} />
-          </div>
         </div>
       </div>
     </div>
