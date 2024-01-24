9fd00c2d21a295182d86cdd7d5afca51806d2ab2
JavidSumra <javidsumra135@gmail.com>
2024-01-24 19:28:18 +0530

    Fixing Issue of Message Persistence by Adding Context

---
+++ client/src/context/Messages/index.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from "react";
+
+const MessagesContext = createContext({
+  messages: [],
+  setMessages: () => {},
+});
+
+const MessagesProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const valueToShare = {
+    messages,
+    setMessages,
+  };
+  return (
+    <MessagesContext.Provider value={valueToShare}>
+      {children}
+    </MessagesContext.Provider>
+  );
+};
+export { MessagesContext, MessagesProvider };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { EraserSizeProvider } from "./context/EraserSize/index.jsx";
+import { MessagesProvider } from "./context/Messages/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <EraserSizeProvider>
-      <App />
+      <MessagesProvider>
+        <App />
+      </MessagesProvider>
     </EraserSizeProvider>
   </Router>
 );
